8e66d74191a4161f30fc49af1381a1a7ed6d7a4e
Tito Osadebe <osadebe.tito@gmail.com>
2022-07-12 15:27:44 -0700

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,19 +8,16 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i, len = 0;
+	int i = 0;
 
-	while (src[i++])
-	{
-		len++;
-	}
-	for (i = 0; i < len && src[i] != '\0'; i++)
+	while (src[i])
 	{
 		dest[i] = src[i];
-	}
-	for (; i < len; i++)
-	{
-		dest[i] = '\0';
+		i++;
+		if (i > 0)
+		{
+			src[i - 1] = '\0';
+		}
 	}
 	return (dest);
 }
