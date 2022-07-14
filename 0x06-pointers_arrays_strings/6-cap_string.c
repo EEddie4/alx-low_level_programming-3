3a22f971fd3bb1402efecd6d1555f484a6da28aa
Tito Osadebe <osadebe.tito@gmail.com>
2022-07-14 11:00:00 -0700

    more pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,8 +7,8 @@
 char *cap_string(char *s)
 {
 	int i, j;
-	char sep[13] = {' ', '\t', '\n', ',', ';',\
-		'.', '!', '?', '"', '(', ')', '{', '}'};
+	char sep[13] = {' ', '\t', '\n', ',', \
+		';','.', '!', '?', '"', '(', ')', '{', '}'};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
@@ -18,7 +18,7 @@ char *cap_string(char *s)
 		}
 		for (j = 0; j < 13; j++)
 		{
-			if (s[i] == separators[j])
+			if (s[i] == sep[j])
 			{
 				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 				{
