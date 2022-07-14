331a2e452a342fba74b9c76f88759e9c5b80085a
Tito Osadebe <osadebe.tito@gmail.com>
2022-07-14 10:41:05 -0700

    more pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,35 +4,26 @@
  * @s: string to be capitalized.
  * Return: s
  */
-int check_separators(char c)
+char *cap_string(char *s)
 {
-	i = 0;
+	int i = 0, j = 0;
 	char separators[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"',\
-		 '(', ')', '{', '}'};
+		'(', ')', '{', '}'};
 
-	for (; i < 13; i++)
-	{
-		if (c == separators[i])
-		{
-			return (1);
-		}
-	}
-	return (0);
-}
-
-char *cap_string(char *)
-{
-	int i = 0;
-
-	while (s[i])
+	while (s[i] != '\0')
 	{
 		if ((s[i] >= 'a' && s[i] <= 'z') && i == 0)
 		{
 			s[i] -= 32;
 		}
-		if (check_separators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+		for (; j < 13; j++)
 		{
-			s[i + 1] -= 32;
+			if (s[i] == separators[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
 		}
 		i++;
 	}
