2260ad9172990ac04b182d0c36a3d2ac904175c5
Tito Osadebe <osadebe.tito@gmail.com>
2022-07-14 10:22:31 -0700

    more pointers, arrays and strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reversed_array(int *a, int n);
-void *string_toupper(char *s);
+char *string_toupper(char *s);
 char *leet(char *s);
 char *cap_string(char *);
 char *_strncpy(char *dest, char *src, int n);
