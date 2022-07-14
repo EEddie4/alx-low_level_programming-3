77e48d37e348c1a9aa5adcf73249f545f5bdfc23
Tito Osadebe <osadebe.tito@gmail.com>
2022-07-14 10:16:20 -0700

    more pointers, arrays and strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,15 +6,14 @@
  */
 char *string_toupper(char *s)
 {
-	int i = 0;
+	int i;
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
 		{
 			s[i] = s[i] - 32;
 		}
-		i++;
 	}
 	return (s);
 }
