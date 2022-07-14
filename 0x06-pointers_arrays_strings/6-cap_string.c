dfe24c55944d8a8405a6322f8ec11bcbef11c039
Tito Osadebe <osadebe.tito@gmail.com>
2022-07-14 09:14:39 -0700

    more pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,17 +20,17 @@ int check_separators(char c)
 	return (0);
 }
 
-char *cap_string(char *)
+char *cap_string(char *s)
 {
 	int i = 0;
 
 	while (s[i])
 	{
-		if (s[i] >= 'a' && s[i] <= 'z' && i == 0)
+		if ((s[i] >= 'a' && s[i] <= 'z') && i == 0)
 		{
 			s[i] -= 32;
 		}
-		if (check_separators(s[i]) && s[i + 1] >= 'a' && s[i + 1] <= 'z')
+		if (check_separators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
 		{
 			s[i + 1] -= 32;
 		}
