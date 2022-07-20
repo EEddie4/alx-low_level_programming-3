c55158c84272c9b51031780217675019aca496c8
Tito Osadebe <osadebe.tito@gmail.com>
2022-07-20 08:21:33 -0700

    Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string to be printed.
+ * Return: Nothing.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_putchar(s + 1);
+	}
+	_putchar('\n');
+	else
+		_putchar('\n');
+}
