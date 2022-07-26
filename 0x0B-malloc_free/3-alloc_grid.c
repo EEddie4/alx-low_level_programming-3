99984f51469fa6b4bea47e7f7f17fa050039ea03
Tito Osadebe <osadebe.tito@gmail.com>
2022-07-26 04:25:54 -0700

    Malloc Free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,24 +10,29 @@
 int **alloc_grid(int width, int height)
 {
 	int **twoDim;
-	int hind, wind;
+	int hind = 0, wind = 0;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	twoDim = malloc(sizeof(int *) * height);
 	if (twoDim == NULL)
 		return (NULL);
-	for (hind = 0; hind < height; hind++)
+	for (; hind < height; hind++)
 	{
-		twoDim = malloc(sizeof(int) * width);
+		twoDim[hind] = malloc(sizeof(int) * width);
 		if (twoDim[hind] == NULL)
 		{
+			for (; hind >= 0; hind--)
+			{
+				free(twoDim[hind]);
+			}
+			free(twoDim);
 			return (NULL);
 		}
 	}
 	for (hind = 0; hind < height; hind++)
 	{
-		for (wind = 0; wind < width; wind++)
+		for (; wind < width; wind++)
 			twoDim[hind][wind] = 0;
 	}
 	return (twoDim);
