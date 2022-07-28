06834c667a331909364d52867ccf5e11d58984e5
Tito Osadebe <osadebe.tito@gmail.com>
2022-07-28 07:30:12 -0700

    More Malloc, Free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,26 +12,31 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *newstr;
-	unsigned int len1 = n, i = 0;
+	unsigned int len1 = 0, len2 = 0, i = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (*s2)
+	while (s1[i])
 		len1++;
-	newstr = malloc(sizeof(char) + (len1 + 1));
+	while (s2[i])
+		len2++;
+	if (n >= len2)
+		n = len2;
+	else
+		return (n);
+	newstr = malloc(sizeof(char) + (len1 + n + 1));
 	if (newstr == NULL)
 		return (NULL);
-	len1 = 0;
 	for (i = 0; s1[i]; i++)
 	{
-		newstr[len1++] = s1[i];
+		newstr[i] = s1[i];
 	}
-	for (i = 0; s2[i] && (i < n); i++)
+	for (i = 0; (s2[i] != '\0') && (i < n); i++)
 	{
 		newstr[len1++] = s2[i];
 	}
-	newstr[len1] = '\0';
+	newstr[len1++] = '\0';
 	return (newstr);
 }
